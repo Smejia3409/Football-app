807908aa9532f9a5d793281db28ee4f401b42d47
Sandro Mejia <sandromejia3409@gmail.com>
2023-03-27 22:23:38 -0400

    fixed dotenv error

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,12 +1,13 @@
 import { connect } from "mongoose";
-require("dotenv").config();
 import * as dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "backend/config/.env" });
 
 export const connectDb = async () => {
+  let connection: any = process.env.MONGO_URI;
+
   try {
-    const conn = await connect("env file");
+    const conn = await connect(connection);
     console.log(`Mongoose connected ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
