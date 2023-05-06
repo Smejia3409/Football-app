7dfd527357d72b52015123478925c4845af4377f
Sandro Mejia <sandromejia3409@gmail.com>
2023-05-06 01:32:36 -0400

    added redirect if token wasnt available

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -1,6 +1,24 @@
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const home = () => {
+  const router = useRouter();
+
+  const getCookie = () => {
+    let cookie: any = {};
+    document.cookie.split(";").forEach(function (el) {
+      let [key, value] = el.split("=");
+      cookie[key.trim()] = value;
+    });
+    return cookie;
+  };
+
+  useEffect(() => {
+    if (!getCookie()) {
+      router.push("/");
+    }
+  });
+
   return <div>home</div>;
 };
 
