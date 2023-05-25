efc63fdefc92594a0ee4250a4963a79cec40de06
Sandro Mejia <sandromejia3409@gmail.com>
2023-05-24 23:45:19 -0400

    added new interface for field events

--- frontend/types.ts
+++ frontend/types.ts
@@ -6,3 +6,17 @@ export interface IField {
   lat: string;
   lng: string;
 }
+
+export interface IEvent {
+  _id: mongoose.ObjectId;
+  event: string;
+  field: string;
+  date: string;
+  time: string;
+  description: string;
+  attending: any[];
+  host: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+}
