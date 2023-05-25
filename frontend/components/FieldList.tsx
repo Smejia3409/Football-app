c9954b09c4e5df6157a446766416d98a149903ee
Sandro Mejia <sandromejia3409@gmail.com>
2023-05-25 13:23:09 -0400

    changed field list design

--- frontend/components/FieldList.tsx
+++ frontend/components/FieldList.tsx
@@ -1,22 +1,57 @@
-import { IField } from "@/types";
-import React from "react";
-import { Card, ListGroup } from "react-bootstrap";
+import { IEvent, IField } from "@/types";
+import React, { useState } from "react";
+import { Accordion, Button, Card, Collapse, ListGroup } from "react-bootstrap";
 
-const FieldList = (props: { fields: IField[] }) => {
+const FieldList = (props: { fields: IField[]; events: IEvent[] }) => {
   let fields = props.fields;
+
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <ListGroup>
       {fields.map((field: IField) => {
         return (
-          <>
-            <ListGroup.Item key={field._id.toString()}>
-              <Card.Body>{field.name}</Card.Body>
-            </ListGroup.Item>
+          <Accordion
+            key={field._id.toString()}
+            id={field._id.toString()}
+            defaultActiveKey="0"
+            // onClick={() => {
+            //   if (field._id) {
+            //     field._id.toString();
+            //   }
+            // }}
+            // aria-controls="example-collapse-text"
+            // aria-expanded={open}
+            // variant="light"
+          >
+            <Accordion.Header>{field.name}</Accordion.Header>
+
+            <SelectedField events={props.events} open={open} />
+
             <br />
-          </>
+          </Accordion>
         );
       })}
     </ListGroup>
+  );
+};
+
+const SelectedField = (props: { events: IEvent[]; open: boolean }) => {
+  return (
+    <Accordion.Body>
+      <div>
+        {props.events.map((event: IEvent) => {
+          return (
+            <div key={event._id.toString()}>
+              <h5>{event.event}</h5>
+              <p>{event.date}</p>
+              <p>{event.time}</p>
+              <p>{event.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </Accordion.Body>
   );
 };
 
