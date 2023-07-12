bc5edc28e60b1ee1f556658681b894e0d1326b31
Sandro Mejia <sandromejia3409@gmail.com>
2023-07-11 23:45:15 -0400

    added field get and create requests

---
+++ frontend/pages/api/field.ts
@@ -0,0 +1,47 @@
+import { IFieldModel } from "@/interfaces";
+import { connectDb } from "@/lib/db";
+import Field from "@/models/fieldsModel";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  //connect to database
+  await connectDb();
+
+  // gets request type
+  const { method } = req;
+
+  switch (method) {
+    case `GET`:
+      try {
+        const fields = await Field.find();
+        res.status(200).json(fields);
+      } catch (error) {
+        res.status(400).json(error);
+      }
+
+    case "POST":
+      try {
+        const { name, lat, long } = req.body;
+        if (!name || !lat || !long) {
+          res.status(400).json({
+            message: "Please fill out all fields",
+          });
+
+          throw new Error("Please fill out all fields");
+        }
+
+        await Field.create<IFieldModel>({
+          name: name,
+          lat: lat,
+          long: long,
+        });
+
+        res.status(200).json(`${name} was created`);
+      } catch (error) {
+        res.status(404).json(error);
+      }
+  }
+}
